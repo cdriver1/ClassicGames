fdcbf67ac2061a953a017ebeb885491accd76443
cdriver1 <c.driver96@hotmail.com>
2016-04-21 12:37:21 -0400

    Fixed Sending and recieving.

--- Controller.java
+++ Controller.java
@@ -35,81 +35,11 @@ public class Controller implements Runnable {
 
     @Override
     public void run() {
-        //<editor-fold defaultstate="collapsed" desc=" ${Commentout stuff} ">
-//        Thread sender = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                try {
-//                    DatagramSocket ds;
-//                    ds = new DatagramSocket();
-//                    int sent = 0;
-//                    while (true) {
-//                        try {
-//                            if (sent < outMsgs.size()) {
-//                                String msg = outMsgs.get(outMsgs.size() - 1);
-//                                byte[] outBytes = msg.getBytes(StandardCharsets.UTF_8);
-//                                DatagramPacket dp = new DatagramPacket(outBytes, outBytes.length,
-//                                        InetAddress.getByName("127.0.0.1"), 3500);
-//                                ds.send(dp);
-//                                plOut("Sent dp");
-//                                sent++;
-//                            }
-//                            Thread.sleep(100);
-//                        } catch (InterruptedException ex) {
-//                            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
-//                        } catch (IOException ex) {
-//                            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
-//                        }
-//                    }
-//                } catch (SocketException ex) {
-//                    Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
-//                }
-//            }
-//        });
-//        Thread rcvr = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                try {
-//                    byte[] inBytes = new byte[512];
-//                    DatagramSocket socket = new DatagramSocket(3500);
-//                    while (true) {
-//                        try {
-//                            DatagramPacket packet = new DatagramPacket(inBytes, inBytes.length);
-//                            socket.setSoTimeout(100);
-//                            socket.receive(packet);
-//                            processMessage(new String(packet.getData()));
-//                            plOut(new String(packet.getData()));
-//                        } catch (SocketTimeoutException ste) {
-//                            try {
-//                                Thread.sleep(50);
-//                            } catch (InterruptedException ex) {
-//                                Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
-//                            }
-//                        }
-//                    }
-//                } catch (SocketException ex) {
-//                    Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
-//                } catch (IOException ex) {
-//                    Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
-//                }
-//            }
-//        });
-//        rcvr.start();
-//        sender.start();
-//        
-//        while(true){
-//            try {
-//                Thread.sleep(1500);
-//            } catch (InterruptedException ex) {
-//                Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-//        }
-        //</editor-fold>
         byte[] inBytes = new byte[256];
         String inMsg;
         try {
             DatagramSocket socket = new DatagramSocket(3500);
-
+            int sent = 0;
             while (true) {
                 try {
                     DatagramPacket packet = new DatagramPacket(inBytes, inBytes.length);
@@ -122,17 +52,17 @@ public class Controller implements Runnable {
 
                 DatagramSocket ds;
                 ds = new DatagramSocket();
-                int sent = 0;
+
                 //try {
-                    if (sent < outMsgs.size()) {
-                        String msg = outMsgs.get(outMsgs.size() - 1);
-                        byte[] outBytes = msg.getBytes(StandardCharsets.UTF_8);
-                        DatagramPacket dp = new DatagramPacket(outBytes, outBytes.length, InetAddress.getByName("127.0.0.1"), 3500);
-                        ds.send(dp);
-                        plOut("Sent '" + msg + "'");
-                        sent++;
-                    }
-                    //Thread.sleep(100);
+                if (sent < outMsgs.size()) {
+                    String msg = outMsgs.get(outMsgs.size() - 1);
+                    byte[] outBytes = msg.getBytes(StandardCharsets.UTF_8);
+                    DatagramPacket dp = new DatagramPacket(outBytes, outBytes.length, InetAddress.getByName("127.0.0.1"), 3500);
+                    ds.send(dp);
+                    plOut("Sent '" + msg + "'");
+                    sent++;
+                }
+                //Thread.sleep(100);
                 //} //catch (InterruptedException ex) {
 //                    Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
 //                }
